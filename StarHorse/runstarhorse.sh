ffa12466ef40a05d918f16369924f0e9d11f4b67
Carlos Adean <carlosadean@linea.gov.br>
2018-05-16 13:48:28 -0300

    added starhorse and others

---
+++ StarHorse/runstarhorse.sh
@@ -0,0 +1,8 @@
+. /mnt/eups/linea_eups_setup.sh
+setup scipy 0.11.0+2
+setup numpy 1.8.1+0
+setup python 2.7.3+2
+#export SH_SCRIPT="/home/anna.queiroz/bpg_distances/run_starhorse.py"
+#export PARAMS="/home/anna.queiroz/bpg_distances/param_files/APOGEE-GAIA_PARAMS"
+
+#python $SH_SCRIPT $PARAMS
